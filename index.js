4ade0e1d07a28ae6d0049fe2b28621f1930825ac
MisDW <96786803+MisDW@users.noreply.github.com>
2022-11-17 17:07:49 -0600

    Update index.js
    modificacion de hora!

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ setInterval(() => {
     const [month, day, year]       = [date.getMonth(), date.getDate(), date.getFullYear()];
     const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
     
-    hora.innerHTML = `${12+hour}:`;
+    hora.innerHTML = `${hour}:`;
     minutos.innerHTML = `${minutes}:`;
     segundos.innerHTML = `${seconds}`;
     
